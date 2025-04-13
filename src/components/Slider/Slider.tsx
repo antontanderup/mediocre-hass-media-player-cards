b807758e974fc4c9e9626bcb596550a7c5440d70
Anton Tanderup Adamsen <antontandrup@gmail.com>
2025-04-13 07:56:30 +0200

    DIY the slider instead

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,24 +1,71 @@
 import styled from "@emotion/styled";
+import { ChangeEvent } from "preact/compat";
 
 export type SliderProps = {
   min: number;
   max: number;
   step: number;
   value: number;
-  thumbSize?: HandleSize;
+  sliderSize?: SliderSize;
   onChange: (value: number) => void;
 };
 
-export type HandleSize = "xsmall" | "small" | "medium" | "large";
+export type SliderSize = "xsmall" | "small" | "medium" | "large";
 
-const SliderWrap = styled.div<{ thumbSize?: HandleSize }>`
+const SliderWrap = styled.div<{ sliderSize?: SliderSize }>`
   display: contents;
+  --unselected-color: var(--divider-color);
 
-  > ha-control-slider {
+  input[type="range"] {
+    -webkit-appearance: none;
+    appearance: none;
     width: 100%;
-    --control-slider-thickness: ${props => getHandleSize(props.thumbSize)};
-    --control-slider-border-radius: 0px;
-    --handle-size: 0px; // show-handle="false" is not working
+    height: ${props => getSliderSize(props.sliderSize || "medium")};
+    background: var(--unselected-color);
+    outline: none;
+    border-radius: 6px;
+    overflow: hidden;
+    margin: 0px;
+
+    /* Firefox specific styles */
+    &::-moz-range-track {
+      background: var(--unselected-color);
+      height: ${props => getSliderSize(props.sliderSize || "medium")};
+    }
+
+    &::-moz-range-progress {
+      background: var(--primary-color);
+      height: ${props => getSliderSize(props.sliderSize || "medium")};
+    }
+
+    /* Chrome and other browsers */
+    &::-webkit-slider-runnable-track {
+      -webkit-appearance: none;
+      height: ${props => getSliderSize(props.sliderSize || "medium")};
+      color: var(--primary-color);
+      margin-top: -1px;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      width: 1px;
+      height: ${props => getSliderSize(props.sliderSize || "medium")};
+      background: var(--primary-color);
+      box-shadow: -100vw 0 0 100vw var(--primary-color);
+      border: 0;
+      margin: 0;
+    }
+
+    &::-moz-range-thumb {
+      width: 0;
+      height: 0;
+      opacity: 0;
+      border: none;
+    }
+
+    &:focus {
+      outline: none;
+    }
   }
 `;
 
@@ -27,35 +74,36 @@ export const Slider = ({
   max,
   step,
   value,
-  thumbSize,
+  sliderSize,
   onChange,
 }: SliderProps) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(parseFloat((e.target as HTMLInputElement).value));
+  };
+
   return (
-    <SliderWrap thumbSize={thumbSize}>
-      <ha-control-slider
+    <SliderWrap sliderSize={sliderSize}>
+      <input
+        type="range"
         min={min}
         max={max}
         step={step}
         value={value}
-        show-handle="false"
-        onvalue-changed={e => {
-          console.log(typeof e.detail.value);
-          onChange(parseFloat(e.detail.value));
-        }}
+        onChange={handleChange}
       />
     </SliderWrap>
   );
 };
 
-const getHandleSize = (thumbSize: HandleSize) => {
-  switch (thumbSize) {
+const getSliderSize = (sliderSize: SliderSize) => {
+  switch (sliderSize) {
     case "xsmall":
-      return "16px";
+      return "14px";
     case "small":
-      return "24px";
+      return "20px";
     case "medium":
-      return "28px";
+      return "24px";
     case "large":
-      return "32px";
+      return "28px";
   }
 };
