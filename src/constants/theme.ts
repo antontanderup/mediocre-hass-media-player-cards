a43f8834a437ff773d9204788df352bd01e73683
Anton Tanderup Adamsen <antontandrup@gmail.com>
2025-05-21 18:45:04 +0200

    Add central theme object

---
+++ src/constants/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    dialog:
+      "var(--mmpc-dialog, var(--ha-dialog-surface-background, var(--mdc-theme-surface,#fff)))",
+    onDialog: "var(--mmpc-on-dialog, var(--primary-text-color, #333))",
+    card: "var(--mmpc-card, var(--card-background-color))",
+    onCard: "var(--mmpc-on-card, var(--primary-text-color, #333))",
+    onCardMuted: "var(--mmpc-on-card-muted, var(--secondary-text-color, #666))",
+    onCardDivider:
+      "var(--mmpc-on-card-divider, var(--divider-color, rgba(0, 0, 0, 0.12)))",
+  },
+};
