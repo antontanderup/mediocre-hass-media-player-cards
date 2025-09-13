2fc700a3a1453cbd29a471124a5619b55d82d9d5
Anton Tanderup <antontandrup@gmail.com>
2025-09-13 08:21:42 +0000

    Add config for multi card

--- src/types/config.ts
+++ src/types/config.ts
@@ -61,6 +61,32 @@ export const MediocreMassiveMediaPlayerCardConfigSchema =
     mode: "'panel'|'card'|'in-card'|'popup'", // don't document popup as it only for internal use
   });
 
+export const MediocreMultiMediaPlayerCardConfigSchema = type({
+  type: "string",
+  media_players: type({
+    entity_id: "string",
+    "use_art_colors?": "boolean",
+    "custom_buttons?": type({
+      icon: "string > 0",
+      name: "string > 0",
+    }),
+    "speaker_group_entity_id?": type("string").or("null"), // entity_id of the main speaker incase it's different from the entity_id of the media player
+    "ma_entity_id?": type("string").or("null"), // MusicAssistant entity_id (adds MA specific features (currently search))
+    "ma_favorite_button_entity_id?": type("string").or("null"), // MusicAssistant button entity to mark current song as favorite
+    "search?": {
+      "enabled?": "boolean | null", // Enables regular Home Assistant search_media functionality
+      "show_favorites?": "boolean | null", // Shows favorites no search query has been entered
+      "entity_id?": type("string").or("null"), // entity_id of the media player to search on (optional will fall back to the entity_id of the card)
+      "media_types?": searchMediaTypeSchema.array(),
+    },
+  }).array(),
+  "speaker_group?": {
+    entities: mediaPlayerConfigEntityArray, // entity_ids of the speakers that can be grouped with the main speaker
+  },
+  "grid_options?": "unknown", // Home Assistant grid layout options (passed through without validation)
+  "visibility?": "unknown", // Home Assistant visibility options (passed through without validation)
+});
+
 export type SearchMediaType = typeof searchMediaTypeSchema.infer;
 export type CommonMediocreMediaPlayerCardConfig =
   typeof commonMediocreMediaPlayerCardConfigSchema.infer;
@@ -68,4 +94,6 @@ export type MediocreMediaPlayerCardConfig =
   typeof MediocreMediaPlayerCardConfigSchema.infer;
 export type MediocreMassiveMediaPlayerCardConfig =
   typeof MediocreMassiveMediaPlayerCardConfigSchema.infer;
+export type MediocreMultiMediaPlayerCardConfig =
+  typeof MediocreMultiMediaPlayerCardConfigSchema.infer;
 export type MediaPlayerConfigEntity = typeof mediaPlayerConfigEntity.infer;
