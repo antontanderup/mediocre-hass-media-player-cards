d9a4939a6b27cd9bde7ebb920cdc1f65363b94ae
Anton Tanderup <antontandrup@gmail.com>
2025-09-16 12:52:07 +0000

    WIP Change player controller

--- src/components/MediocreMultiMediaPlayerCard/components/SpeakerGrouping.tsx
+++ src/components/MediocreMultiMediaPlayerCard/components/SpeakerGrouping.tsx
@@ -1,13 +1,17 @@
-import { useContext, useState } from "preact/hooks";
+import { useContext, useMemo, useState } from "preact/hooks";
 import type {
+  MediaPlayerEntity,
   MediocreMultiMediaPlayer,
   MediocreMultiMediaPlayerCardConfig,
 } from "@types";
 import {
+  AlbumArt,
   CardContext,
   CardContextType,
   GroupVolumeController,
   IconButton,
+  PlayerContextProvider,
+  useHass,
 } from "@components";
 import { css } from "@emotion/react";
 import { theme } from "@constants";
@@ -46,6 +50,14 @@ const styles = {
     display: "flex",
     alignItems: "center",
   }),
+  miniPlayer: css({
+    padding: 4,
+    borderRadius: 4,
+    backgroundColor: theme.colors.onCardDivider,
+    display: "flex",
+    alignItems: "center",
+    gap: 8,
+  })
 };
 
 export type SpeakerGroupingProps = {
@@ -57,6 +69,7 @@ export const SpeakerGrouping = ({ mediaPlayer }: SpeakerGroupingProps) => {
     useContext<CardContextType<MediocreMultiMediaPlayerCardConfig>>(
       CardContext
     );
+  const hass = useHass();
 
   const { speaker_group } = config;
   const { entity_id, speaker_group_entity_id } = mediaPlayer;
@@ -67,9 +80,39 @@ export const SpeakerGrouping = ({ mediaPlayer }: SpeakerGroupingProps) => {
   // Use the specified entity_id for the group or fall back to the main entity_id
   const mainEntityId = speaker_group_entity_id || entity_id;
 
+  const enrichedEntities = useMemo(() => {
+    const mainGroupingPlayer = hass.states[mainEntityId];
+    return speaker_group?.entities.map((entity) => {
+      const entityId = typeof entity === "string" ? entity : entity.entity;
+      const player = hass.states[entityId] as MediaPlayerEntity
+      return {
+        ...player,
+        attributes: {
+          ...player?.attributes,
+          friendly_name: typeof entity !== "string" && entity.name ? entity.name : player?.attributes.friendly_name || entityId,
+        },
+        isGrouped: !!player && mainGroupingPlayer.attributes.group_members.includes(entityId),
+        isMainSpeaker: player.entity_id === mainEntityId,
+      }
+    }) ?? [];
+  }, [hass, mainEntityId]);
+  console.log(enrichedEntities)
+
+  const renderPlayer = (player: typeof enrichedEntities[number]) => {
+    if (!player || player.isMainSpeaker) { return null; }
+    return (
+      <PlayerContextProvider key={player.entity_id} hass={hass} entityId={player.entity_id}>
+        <div css={styles.miniPlayer}>
+          <AlbumArt size={24} iconSize="x-small" />
+          <span>{player.attributes.friendly_name}</span>
+        </div>
+      </PlayerContextProvider>
+    );
+  };
+
   return (
     <div css={styles.speakerGroupContainer}>
-      <ViewHeader title="Join Media Players" subtitle="Sync media players and adjust volumes." renderAction={() => (
+      <ViewHeader title="Join media players" subtitle="Selected player grouping." renderAction={() => (
         <div css={styles.syncContainer}>
           <span
             css={styles.syncText}
@@ -83,22 +126,23 @@ export const SpeakerGrouping = ({ mediaPlayer }: SpeakerGroupingProps) => {
             }
             size="x-small"
             onClick={() => setSyncMainSpeakerVolume(!syncMainSpeakerVolume)}
-            />
-            </div>
+          />
+        </div>
       )} />
-      <div css={styles.groupedSpeakers}>
-        <GroupVolumeController
-          config={{
-            entity_id,
-            speaker_group: {
-              entities: speaker_group?.entities || [],
-              entity_id: mainEntityId,
-            },
-          }}
-          syncMainSpeaker={syncMainSpeakerVolume}
-          showUngrouped={true}
-        />
-      </div>
+
+      <GroupVolumeController
+        config={{
+          entity_id,
+          speaker_group: {
+            entities: speaker_group?.entities || [],
+            entity_id: mainEntityId,
+          },
+        }}
+        syncMainSpeaker={syncMainSpeakerVolume}
+        showUngrouped={true}
+      />
+      <ViewHeader title="Change player" subtitle="Change which player you are controlling." />
+      {enrichedEntities.length > 0 && enrichedEntities.map(renderPlayer)}
     </div>
   );
 };
