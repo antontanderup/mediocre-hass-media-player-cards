d7627974dc38382609a6ce1c76fdcf05897dc60d
Anton Tanderup <antontandrup@gmail.com>
2025-09-19 08:33:04 +0000

    expect error for now

--- src/components/MediocreMultiMediaPlayerCard/components/FooterActions.tsx
+++ src/components/MediocreMultiMediaPlayerCard/components/FooterActions.tsx
@@ -7,6 +7,7 @@ import {
   MediocreMultiMediaPlayer,
   MediocreMultiMediaPlayerCardConfig,
 } from "@types";
+// @ts-expect-error i'll allow it for now
 import { NavigationRoute } from "../MediocreMultiMediaPlayerCard";
 import { theme } from "@constants";
 import { useActionProps } from "@hooks";
