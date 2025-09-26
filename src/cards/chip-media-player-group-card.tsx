c5441494681fadd1a9cc1219c71417d85bc04e73
Anton Tanderup <antontandrup@gmail.com>
2025-09-26 08:47:39 +0000

    Bring back hass comparison render blockers

--- src/cards/chip-media-player-group-card.tsx
+++ src/cards/chip-media-player-group-card.tsx
@@ -2,10 +2,25 @@ import {
   MediocreChipMediaPlayerGroupCard,
   MediocreChipMediaPlayerGroupCardConfig,
 } from "@components";
+import { HomeAssistant, MediaPlayerEntity } from "@types";
+import { getDidMediaPlayerUpdate } from "@utils";
 import { CardWrapper } from "@wrappers";
 
 class MediocreChipMediaPlayerGroupCardWrapper extends CardWrapper<MediocreChipMediaPlayerGroupCardConfig> {
   Card = MediocreChipMediaPlayerGroupCard;
+
+  shouldUpdate = (
+    prevHass: HomeAssistant | null,
+    hass: HomeAssistant | null
+  ) => {
+    if (!hass || !prevHass || !this.config) return true;
+    if (!prevHass && hass) return true;
+
+    return getDidMediaPlayerUpdate(
+      prevHass.states[this.config.entity_id] as MediaPlayerEntity,
+      hass.states[this.config.entity_id] as MediaPlayerEntity
+    );
+  };
 
   setConfig(config: MediocreChipMediaPlayerGroupCardConfig) {
     if (!config.entity_id) {
--- src/cards/mediocre-media-player-card.tsx
+++ src/cards/mediocre-media-player-card.tsx
@@ -1,4 +1,4 @@
-import { HomeAssistant } from "@types";
+import { HomeAssistant, MediaPlayerEntity } from "@types";
 import {
   MediocreMediaPlayerCard,
   MediocreMediaPlayerCardProps,
@@ -6,6 +6,7 @@ import {
 import { MediocreMediaPlayerCardConfig } from "@types";
 import { CardWrapper } from "@wrappers";
 import { FC } from "preact/compat";
+import { getDidMediaPlayerUpdate } from "@utils";
 
 class MediocreMediaPlayerCardWrapper extends CardWrapper<MediocreMediaPlayerCardConfig> {
   Card: FC<MediocreMediaPlayerCardProps> = MediocreMediaPlayerCard;
@@ -16,6 +17,57 @@ class MediocreMediaPlayerCardWrapper extends CardWrapper<MediocreMediaPlayerCard
     }
     this.config = config;
   }
+
+  shouldUpdate = (
+    prevHass: HomeAssistant | null,
+    hass: HomeAssistant | null
+  ) => {
+    if (!hass || !prevHass || !this.config) return true;
+    if (!prevHass && hass) return true;
+
+    // Check if main entity changed
+    if (
+      getDidMediaPlayerUpdate(
+        prevHass.states[this.config.entity_id] as MediaPlayerEntity,
+        hass.states[this.config.entity_id] as MediaPlayerEntity
+      )
+    ) {
+      return true;
+    }
+
+    // Check if speaker group entity changed (if configured)
+    if (
+      this.config.speaker_group?.entity_id &&
+      getDidMediaPlayerUpdate(
+        prevHass.states[
+          this.config.speaker_group.entity_id
+        ] as MediaPlayerEntity,
+        hass.states[this.config.speaker_group.entity_id] as MediaPlayerEntity
+      )
+    ) {
+      return true;
+    }
+
+    if (this.config.speaker_group?.entities) {
+      for (const entity of this.config.speaker_group.entities) {
+        if (
+          getDidMediaPlayerUpdate(
+            prevHass.states[
+              typeof entity === "string" ? entity : entity.entity
+            ] as MediaPlayerEntity,
+            hass.states[
+              typeof entity === "string" ? entity : entity.entity
+            ] as MediaPlayerEntity,
+            true
+          )
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
 
   static getConfigElement() {
     return document.createElement(
