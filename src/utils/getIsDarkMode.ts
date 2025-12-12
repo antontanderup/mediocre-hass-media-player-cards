aef8740c17d9945f6bf5d77125e1c8dae643ba47
Anton Tanderup <antontandrup@gmail.com>
2025-12-12 19:47:47 +0000

    also make it work for auto switching themes

--- src/utils/getIsDarkMode.ts
+++ src/utils/getIsDarkMode.ts
@@ -1,3 +1,11 @@
 import { getHass } from "@utils";
 
-export const isDarkMode = () => !!getHass().selectedTheme?.dark;
+export const isDarkMode = () => {
+  if (typeof getHass().selectedTheme?.dark === "boolean") {
+    return getHass().selectedTheme?.dark;
+  }
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+};
