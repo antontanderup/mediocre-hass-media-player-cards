fff5d4cf4f82cc901bd947bcb9e07fbf8faa6d95
Anton Tanderup <antontandrup@gmail.com>
2026-02-15 11:02:15 +0000

    Delete unused components

--- src/utils/getMultiConfigToMediocreMassiveConfig.ts
+++ src/utils/getMultiConfigToMediocreMassiveConfig.ts
@@ -8,6 +8,21 @@ export const getMultiConfigToMediocreMassiveConfig = (
   selectedPlayer: MediocreMultiMediaPlayerCardConfig["media_players"][number],
   mode: "panel" | "card" | "in-card" | "popup" | "multi"
 ): MediocreMassiveMediaPlayerCardConfig => {
+  const speaker_group = {
+    entity_id: selectedPlayer.speaker_group_entity_id,
+    entities: config.media_players
+      .filter(player => player.can_be_grouped)
+      .map(player => {
+        if (player.name) {
+          return {
+            name: player.name,
+            entity: player.speaker_group_entity_id ?? player.entity_id,
+          };
+        } else {
+          return player.speaker_group_entity_id ?? player.entity_id;
+        }
+      }),
+  };
   return {
     type: "custom:mediocre-massive-media-player-card",
     entity_id: selectedPlayer.entity_id,
@@ -20,21 +35,8 @@ export const getMultiConfigToMediocreMassiveConfig = (
     media_browser: selectedPlayer.media_browser,
     custom_buttons: selectedPlayer.custom_buttons,
     mode: mode,
-    speaker_group: {
-      entity_id: selectedPlayer.speaker_group_entity_id,
-      entities: config.media_players
-        .filter(player => player.can_be_grouped)
-        .map(player => {
-          if (player.name) {
-            return {
-              name: player.name,
-              entity: player.speaker_group_entity_id ?? player.entity_id,
-            };
-          } else {
-            return player.speaker_group_entity_id ?? player.entity_id;
-          }
-        }),
-    },
+    speaker_group:
+      speaker_group.entities.length > 1 ? speaker_group : undefined,
     options: {
       show_volume_step_buttons:
         config.options?.show_volume_step_buttons ?? false,
